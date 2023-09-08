1dd560cd178be1f2ce6cc2d839b63bf3ea803951
DEIVIS JESID OLASCOAGA SALCEDO <deivisolascoagajs@unimagdalena.edu.co>
2023-09-07 19:55:22 -0500

    Cree el modulo donde estan los metodos que seran usado para ralizar polimorfismo a lenguaje y alfabeto

---
+++ set_operations.py
@@ -0,0 +1,15 @@
+class SetOperations:
+    def _init_(self, data):
+        self.data = set(data)
+
+    def union(self, other_set):
+        return SetOperations(self.data.union(other_set.data))
+
+    def difference(self, other_set):
+        return SetOperations(self.data.difference(other_set.data))
+
+    def intersection(self, other_set):
+        return SetOperations(self.data.intersection(other_set.data))
+
+    def cardinality(self):
+        return len(self.data)
