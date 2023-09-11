667f6e0662845186b8b4daec22a35e08761d2169
DEIVIS JESID OLASCOAGA SALCEDO <deivisolascoagajs@unimagdalena.edu.co>
2023-09-10 23:50:13 -0500

    mejore algunas funcionalidades del menu alfabeto, solo falta soluccionar las llamdas de las operaciones en la linea 76

--- alphabet_menu.py
+++ alphabet_menu.py
@@ -1,5 +1,7 @@
-from alphabet import Alphabet
+from Alphabet import Alphabet
 import re
+import time
+import os
 
 def process_set(alphabets):
     
@@ -9,17 +11,43 @@ def process_set(alphabets):
     for sets in all_alphabets:
         name = sets[0]  # Nombre del conjunto
         values = re.findall(r'[^,\s]+', sets[1])  # Lista de valores del conjunto
-        Alphabet.all_alphabet[name] = Alphabet(values)
+        Alphabet.all_alphabet[name] = values
         
+
   
+def show_all_alphabet():
+    print(f"Los Alfabetos registrados hasta el momento son: ")
+    print(Alphabet.get_all_alphabet())
+
+
+def select_sets():
+    pass
+
+
+
 def alphabet_menu():
-    
-    language_input = input("Digite los conjuntos de alfabeto de la forma A = {f, k, 3} B = {h, 5, h, 2}")
-    language_input = "A = {f, k, 3} B = {h, 5, h, 2} C = {a, l, 3} C = {h, 5, t, jk}: "
+
+
+    os.system('cls')
+    language_input = input("Digite los conjuntos de alfabeto de la forma A = {f, k, 3} B = {h, 5, h, 2}\n")
+
+    #language_input = "A = {f, k, 3} B = {h, 5, h, 2} C = {a, l, 3} C = {h, 5, t, jk}: "   para que se guardan conjuntos de manera manual, si arriba los estas pidiendo????
+
     process_set(language_input)
+    os.system('cls')
     
     
     while True:
+
+        os.system('cls')
+
+        show_all_alphabet()
+        alphabet_names = input("\n\ningrese los nombres de los conjuntos a operar de la forma A B C o A, B, C\n")
+        alphabet_names = re.findall(r'[^,\s]+', alphabet_names)
+
+
+        os.system('cls')
+
         print("\n    Menú de Operaciones con Alfabetos    ")
         print("1. Unión")
         print("2. Intersección")
@@ -28,14 +56,18 @@ def alphabet_menu():
         print("5. Volver al menú principal")
         choice = input("Selecciona una operación: ")
         
+        os.system('cls')
+        
+        '''
+        esto para q???????? 
         
         print("Alfabetos disponibles para la operación: ")
         for name in Alphabet.all_alphabet:
             print(f"{name}: {Alphabet.all_alphabet[name].elements}")
-        alphabet_names = input("ingrese los nombres de los conjuntos a operar de la forma A B C o A, B, C")
-        alphabet_names = re.findall(r'[^,\s]+', alphabet_names)
-       
-       
+            
+            
+        '''
+
         result = None
 
         for name in alphabet_names:
